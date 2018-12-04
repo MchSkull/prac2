0559b8a1581779e23ea0924e1f51f499b8d2b027
José Miguel Chávez <43799628+MchSkull@users.noreply.github.com>
2018-12-04 10:03:52 -0600

    Add files via upload

---
+++ Practica 2/Practica2/Practica2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica2
+//
+//  Created by I104-27 on 28/08/18.
+//  Copyright © 2018 I104-27. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
